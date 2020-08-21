4e6dbb8dcd986768c2c447d7a830fda30a06212f
Ruaxin <qy.xin@foxmail.com>
2020-08-21 19:09:14 +0800

    创建第一个组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>hi</div>
+  <My/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import My from './components/My.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      My
+    }
   }
-}
 </script>
