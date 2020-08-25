2fb1f770fd10d6f8b9011b3d0b08bc1ed713d043
Ruaxin <qy.xin@foxmail.com>
2020-08-25 15:09:27 +0800

    Home样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,11 +13,12 @@
 </template>
 
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
+  import Topnav from '../components/Topnav.vue';
+
   export default {
     name: 'Home',
-    components:{Topnav}
-  }
+    components: {Topnav}
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -39,6 +40,31 @@
 
       > li {
         margin: 0 1em;
+      }
+    }
+  }
+
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: #e57d90;
+
+    > .actions {
+      padding: 8px 0;
+
+      a {
+        margin: 0 8px;
+        background: #555553;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+        color: #fffeff;
       }
     }
   }
