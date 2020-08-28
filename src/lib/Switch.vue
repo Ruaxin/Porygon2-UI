36781fef6a448efd2a2d1f8be777f06439cef56e
Ruaxin <qy.xin@foxmail.com>
2020-08-28 11:05:46 +0800

    button在点击之后可以切换

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,11 +1,22 @@
 <template>
-  <button><span></span></button>
+  <button :class="{checked}" @click="toggle">
+    <span></span>
+  </button>
 </template>
 
-<script>
+<script lang="ts">
+  import {ref} from 'vue';
+
   export default {
-    name: 'Switch'
-  }
+    name: 'Switch',
+    setup() {
+      const checked = ref(true);
+      const toggle = () => {
+        checked.value = !checked.value;
+      };
+      return {checked, toggle};
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -15,7 +26,7 @@
     height: $h;
     width: $h*2;
     border: none;
-    background: #5489ae;
+    background: #b4b0af;
     border-radius: $h/2;
     position: relative;
   }
@@ -30,8 +41,12 @@
     border-radius: $h2/2;
   }
 
-  button:hover > span {
-    left: calc(100% - #{$h2} - 2px)
+  button.checked {
+    background: #5489ae;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px)
+    }
   }
 
 </style>
