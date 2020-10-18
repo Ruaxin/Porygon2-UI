51eaf676c650fd9d19e8d6e5b48881322bf8f71f
Ruaxin <qy.xin@foxmail.com>
2020-10-18 19:20:08 +0800

    用v-model实现初始值的传递和联动

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <Switch/>
+    <Switch v-model:value="initValue"/>
   </div>
 </template>
 
 <script lang="ts">
-  import Switch from '../lib/Switch.vue';
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 
-  export default {
-    name: 'SwitchDemo',
-    components: {Switch}
-  };
+export default {
+  name: 'SwitchDemo',
+  components: {Switch},
+  setup() {
+    const initValue = ref(true);
+    return {initValue};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,57 +1,58 @@
 <template>
-  <button :class="{checked}" @click="toggle">
+  <button :class="{checked:value}" @click="toggle">
     <span></span>
   </button>
 </template>
 
 <script lang="ts">
-  import {ref} from 'vue';
 
-  export default {
-    name: 'Switch',
-    setup() {
-      const checked = ref(false);
-      const toggle = () => {
-        checked.value = !checked.value;
-      };
-      return {checked, toggle};
-    }
-  };
+export default {
+  name: 'Switch',
+  props: {
+    value: Boolean
+  },
+  setup(props: any, context: any) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+    return {toggle};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  $h: 22px;
-  $h2: $h - 4px;
-  button {
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: #b4b0af;
-    border-radius: $h/2;
-    position: relative;
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: #b4b0af;
+  border-radius: $h/2;
+  position: relative;
 
-    &:focus {
-      outline: none;
-    }
+  &:focus {
+    outline: none;
   }
+}
 
-  span {
-    height: $h2;
-    width: $h2;
-    position: absolute;
-    background: #fffeff;
-    top: 2px;
-    left: 2px;
-    border-radius: $h2/2;
-    transition: left 250ms;
+span {
+  height: $h2;
+  width: $h2;
+  position: absolute;
+  background: #fffeff;
+  top: 2px;
+  left: 2px;
+  border-radius: $h2/2;
+  transition: left 250ms;
+}
+
+button.checked {
+  background: #5489ae;
+
+  > span {
+    left: calc(100% - #{$h2} - 2px)
   }
-
-  button.checked {
-    background: #5489ae;
-
-    > span {
-      left: calc(100% - #{$h2} - 2px)
-    }
-  }
+}
 
 </style>
