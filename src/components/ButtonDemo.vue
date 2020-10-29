41048ed50533fcf88e86e4d15327ef3a74722fec
Ruaxin <qy.xin@foxmail.com>
2020-10-29 16:53:30 +0800

    支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,19 +9,39 @@
   </div>
   <h1>示例2</h1>
   <div>
-    <Button size="big">卡比兽</Button>
-    <Button>多边兽</Button>
-    <Button size="small">弱丁鱼</Button>
+    <div class="wrapper">
+      <Button size="big">卡比兽</Button>
+      <Button>多边兽</Button>
+      <Button size="small">弱丁鱼</Button>
+    </div>
+    <div class="wrapper">
+      <Button size="big" theme="link">卡比兽</Button>
+      <Button theme="link">多边兽</Button>
+      <Button size="small" theme="link">弱丁鱼</Button>
+    </div>
+    <div class="wrapper">
+      <Button size="big" theme="text">卡比兽</Button>
+      <Button theme="text">多边兽</Button>
+      <Button size="small" theme="text">弱丁鱼</Button>
+    </div>
   </div>
+  <h1>示例3</h1>
   <div>
-    <Button size="big" theme="link">卡比兽</Button>
-    <Button theme="link">多边兽</Button>
-    <Button size="small" theme="link">弱丁鱼</Button>
-  </div>
-  <div>
-    <Button size="big" theme="text">卡比兽</Button>
-    <Button theme="text">多边兽</Button>
-    <Button size="small" theme="text">弱丁鱼</Button>
+    <div class="wrapper">
+      <Button level="main">高级球</Button>
+      <Button>精灵球</Button>
+      <Button level="danger">精灵球</Button>
+    </div>
+    <div class="wrapper">
+      <Button level="main" theme="link">高级球</Button>
+      <Button theme="link">精灵球</Button>
+      <Button level="danger" theme="link">精灵球</Button>
+    </div>
+    <div class="wrapper">
+      <Button level="main" theme="text">高级球</Button>
+      <Button theme="text">精灵球</Button>
+      <Button level="danger" theme="text">精灵球</Button>
+    </div>
   </div>
 </template>
 
@@ -41,5 +61,7 @@ export default {
 </script>
 
 <style scoped>
-
+.wrapper{
+  margin: 10px 0;
+}
 </style>
